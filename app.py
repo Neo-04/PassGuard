90a6de3c4931b35336a60c7fdf285adfebdb8544
Aditya Balaji <adit.bal.007@gmail.com>
2026-04-13 19:46:08 +0530

    add deployment configuration

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import re
+import os
 
 app = Flask(__name__)
 
@@ -40,4 +41,5 @@ def check():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
